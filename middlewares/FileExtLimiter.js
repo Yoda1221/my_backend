e6a464651df863d8fffc6f24343136eb8e4748ad
Yoda1221 <bfekete66@gmail.com>
2023-02-04 13:17:24 +0100

    change girignore file

---
+++ middlewares/FileExtLimiter.js
@@ -0,0 +1,20 @@
+const path = require("path")
+
+const FileExtLimiter = (allowedExtArray) => {
+    return (req, res, next) => {
+        const files = req.files
+        const fileExtensions = []
+        Object.keys(files).forEach(key => {
+            fileExtensions.push(path.extname(files[key].name))
+        })
+        //  ARE THE FILE EXTENSION IS ALLOVWED? 
+        const allowed = fileExtensions.every(ext => allowedExtArray.includes(ext))
+        if (!allowed) {
+            const message = `Upload failed. Only ${allowedExtArray.toString()} files allowed.`.replaceAll(",", ", ");
+            return res.status(422).json({ status: "error", message });
+        }
+        next()
+    }
+}
+
+export default FileExtLimiter
